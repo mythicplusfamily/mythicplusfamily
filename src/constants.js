2072339952df2e0ba82c915cb920fb03e3c1c9d1
Brandon Jernigan <brandon@whatisboom.com>
2024-05-25 20:21:48 -0500

    refactored to use parcel/tailwind/react, added IO leaderboard

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const classColors = {
+  'Death Knight': '#C41F3B',
+  'Demon Hunter': '#A330C9',
+  'Druid': '#FF7D0A',
+  'Evoker': '#33937F',
+  'Hunter': '#ABD473',
+  'Mage': '#69CCF0',
+  'Monk': '#00FF96',
+  'Paladin': '#F58CBA',
+  'Priest': '#FFFFFF',
+  'Rogue': '#FFF569',
+  'Shaman': '#0070DE',
+  'Warlock': '#9482C9',
+  'Warrior': '#C79C6E'
+};
